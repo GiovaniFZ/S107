f8651f2fad99085c3d6727d3ad03a28919c6d2c1
GiovaniFZ <giovanifinazzi@gmail.com>
2024-09-06 16:14:39 -0300

    conf: Preparar os testes do Mockito

--- src/test/java/org/atendimento/mokito/TestAtendimento.java
+++ src/test/java/org/atendimento/mokito/TestAtendimento.java
@@ -1,8 +1,22 @@
 package org.atendimento.mokito;
 
+import org.atendimento.AtendimentoService;
+import org.atendimento.BuscaAtendimento;
+import org.junit.Before;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
 
 @RunWith(MockitoJUnitRunner.class)
 public class TestAtendimento {
 
+    @Mock
+    private AtendimentoService atendimentoService;
+    @Mock
+    private BuscaAtendimento buscaAtendimento;
+
+    @Before
+    public void setup(){
+        buscaAtendimento = new BuscaAtendimento(atendimentoService);
+    }
 }
