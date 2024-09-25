4b2ea822f4ab6e0b657a57b4af49d7826770b88e
MvLeronn <marcos23_correia@hotmail.com>
2024-09-25 02:40:11 -0300

    Feito pronto para testar

--- scripts/send_email.py
+++ scripts/send_email.py
@@ -1,39 +1,27 @@
-import smtplib
-from email.mime.text import MIMEText
-from email.mime.multipart import MIMEMultipart
+import os
+import subprocess
 
-def send_email():
-    # Informações do servidor de e-mail (SMTP)
-    smtp_server = "smtp.gmail.com"  # Servidor SMTP, aqui usado o Gmail
-    smtp_port = 587  # Porta SMTP
-    email_sender = "seu-email@gmail.com"
-    email_password = "sua-senha-aqui"
-    
-    # Informações do destinatário
-    email_recipient = "destinatario@gmail.com"
-    
-    # Criação do conteúdo do e-mail
-    subject = "Informações sobre a execução do Pipeline"
+def send_email(to_email):
+    subject = "Pipeline Executado"
     body = "Pipeline executado com sucesso!"
-    
-    # Criação da mensagem
-    msg = MIMEMultipart()
-    msg['From'] = email_sender
-    msg['To'] = email_recipient
-    msg['Subject'] = subject
-    
-    # Anexar o corpo do e-mail
-    msg.attach(MIMEText(body, 'plain'))
 
-    try:
-        # Conectar ao servidor SMTP e enviar o e-mail
-        server = smtplib.SMTP(smtp_server, smtp_port)
-        server.starttls()  # Iniciar TLS para segurança
-        server.login(email_sender, email_password)  # Login no servidor SMTP
-        text = msg.as_string()
-        server.sendmail(email_sender, email_recipient, text)  # Enviar o e-mail
-        print("E-mail enviado com sucesso!")
-    except Exception as e:
-        print(f"Erro ao enviar e-mail: {str(e)}")
-    finally:
-        server.quit()
+    # Compor a mensagem
+    message = f"Subject: {subject}\n\n{body}"
+
+    # Usar sendmail para enviar o e-mail
+    process = subprocess.Popen(
+        ["/usr/sbin/sendmail", to_email],
+        stdin=subprocess.PIPE,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE
+    )
+    stdout, stderr = process.communicate(input=message.encode())
+
+    if process.returncode == 0:
+        print(f"E-mail enviado com sucesso para {to_email}")
+    else:
+        print(f"Falha ao enviar e-mail: {stderr.decode()}")
+
+if __name__ == "__main__":
+    commit_author_email = os.getenv('COMMIT_AUTHOR_EMAIL')
+    send_email(commit_author_email)
